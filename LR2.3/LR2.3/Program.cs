da24350ecd3a8f49b3c6313a42d061b0dce357ab
Pavel <79203887+PaulCh4@users.noreply.github.com>
2021-03-05 18:48:34 +0300

    Add files via upload

---
+++ LR2.3/LR2.3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+
+namespace LR2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            StringBuilder text = new StringBuilder("ajxyieualtirctbayultwuyrabyzikyudfilctayrxlnwiafhusd,tcyilyxiaofhnguailtyux.nHz.xtu,zgbhjcdtyara;io;zuitryicwtau");
+            StringBuilder vowels = new StringBuilder("eyuioa ");
+
+            Console.WriteLine("Дана строка, состоящая из строчных английских букв. Заменить в ней все буквы, стоящие после гласных, на следующие по алфавиту (z заменяется на a).");
+            Console.WriteLine(text);
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < vowels.Length; j++)
+                {
+                    if(text[i]==vowels[j])
+                    {
+                        if (text[i + 1] == 'z')
+                        {
+                            text[i + 1] = 'a';
+                            continue;
+                        }
+                        text[i+1] = Convert.ToChar(Convert.ToInt32(text[i+1])+1);
+                    }
+                }
+            }
+
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
