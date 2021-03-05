e5a527eff0d5f589db1da909dc6615e69ffdb479
Pavel <79203887+PaulCh4@users.noreply.github.com>
2021-03-05 18:47:53 +0300

    Add files via upload

---
+++ LR2.1/LR2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LR2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Tobias Booon Noob Saibot";
+
+            Console.WriteLine("В заданной строке поменять порядок слов на обратный (слова разделены пробелами).");
+            Console.WriteLine(text);
+
+            string[] words = text.Split(' ');
+            Console.WriteLine("reversed:");
+            Array.Reverse(words);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.Write(words[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+} 
+
